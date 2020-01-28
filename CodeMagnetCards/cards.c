a7a6212aa566b6a4008801368c8350df8a4aae9c
AlainDlcTVOP <alain_dlc@outlook.com>
2020-01-28 19:02:58 +0100

    Chapter one

--- CodeMagnetCards/cards.c
+++ CodeMagnetCards/cards.c
@@ -1,38 +1,42 @@
 #include <stdio.h>
 #include <stdlib.h>
-
-int main(int argc, char const *argv[])
+int main()
 {
     char card_name[3];
-    puts("Enter the card_name: ");
-    scanf("%2s", card_name);
-    int val = 0;
-    switch (card_name[0])
+    int count = 0;
+    while (card_name[0] != 'X')
+
     {
-    case 'K':
-    case 'Q':
-    case 'J':
-        val = 10;
-        break;
-    case 'A':
-        val = 11;
-         break;
-    default:
-        val = atoi(card_name);
+        puts("Enter the card_name: ");
+        scanf("%2s", card_name);
+        int val = 0;
+        switch (card_name[0])
+        {
+        case 'K':
+        case 'Q':
+        case 'J':
+            val = 10;
+            break;
+        case 'A':
+            val = 11;
+            break;
+        case 'X':
+            continue;
+        default:
        
+            val = atoi(card_name);
+             if((val < 1 ) && (val > 10) )
+             continue;
+        }
+        if ((val > 2) && (val < 7))
+        {
+            count++;
+        }
+        else if (val == 10)
+        {
+            count--;
+        }
+        printf("Current count: %i\n", count);
     }
-
-    printf("The card value is: %i\n", val);
-    /* Check if the value is 3 to 6 */
-    if ((val > 2) && (val < 7))
-    {
-        puts("Count has gone up");
-    }
-    /* Otherwise check if the card was 10, J, Q, or K */
-    else if (val == 10)
-    {
-        puts("Count has gone down");
-    }
-
     return 0;
 }
