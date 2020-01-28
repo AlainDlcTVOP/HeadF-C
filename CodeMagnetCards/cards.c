91ffecdfdcbd3908e13a522021638470474c1ae7
AlainDlcTVOP <alain_dlc@outlook.com>
2020-01-28 18:19:44 +0100

    Cards Modify Swith

--- CodeMagnetCards/cards.c
+++ CodeMagnetCards/cards.c
@@ -7,29 +7,24 @@ int main(int argc, char const *argv[])
     puts("Enter the card_name: ");
     scanf("%2s", card_name);
     int val = 0;
-    if (card_name[0] == 'K')
+    switch (card_name[0])
     {
+    case 'K':
+    case 'Q':
+    case 'J':
         val = 10;
-    }
-    else if (card_name[0] == 'Q')
-    {
-        val = 10;
-    }
-    else if (card_name[0] == 'J')
-    {
-        val = 10;
-    }
-    else if (card_name[0] == 'A')
-    {
+        break;
+    case 'A':
         val = 11;
-    }
-    else
-    {
+         break;
+    default:
         val = atoi(card_name);
+       
     }
+
     printf("The card value is: %i\n", val);
     /* Check if the value is 3 to 6 */
-    if ((val >2) || (val < 7))
+    if ((val > 2) && (val < 7))
     {
         puts("Count has gone up");
     }
@@ -40,6 +35,4 @@ int main(int argc, char const *argv[])
     }
 
     return 0;
-
-    
 }
